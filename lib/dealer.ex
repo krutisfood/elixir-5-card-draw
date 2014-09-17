1d11c1de71150e505265e4e4699d673765060bdc
Kurt Gardiner <k.a.gardiner@gmail.com>
2014-09-18 08:17:44 +1000

    Deal now deals!

--- lib/dealer.ex
+++ lib/dealer.ex
@@ -1,17 +1,44 @@
 defmodule Dealer do
-
   def deck do
     values = Stream.cycle(2..14) |> Enum.take 52
     suits = Stream.cycle([:hearts,:diamonds,:spades,:clubs]) |> Enum.take 52
     deck = values |> Enum.zip suits
   end
 
-  def deal_one_from(deck) do
-    deck |> Enum.take 1
+  def shuffle(deck) do
+    Enum.shuffle deck
   end
 
-  def deal_hands(no_players, deck) do
-    deal_one_from deck
-    # terminate when all plays have seven cards...
+  def deal(no_players, deck) do
+    six_players = 6
+    no_cards_required = six_players * 5
+    deck |> Enum.take(no_cards_required) |> Enum.chunk 5
+  end
+
+  # straight flushes
+  def name_hand([{a,s}, {b,s}, {c,s}, {d,s}, {e,s}]) when a == (b+1) and b == (c+1) and c == (d+1) and d == (e+1), do: :straight_flush
+  def name_hand([{a,s}, {b,s}, {c,s}, {d,s}, {e,s}]) when a == 14 and b == 5 and b == (c+1) and c == (d+1) and d == (e+1), do: :straight_flush
+
+  # four of a kind
+  def name_hand([{a,_}, {a,_}, {a,_}, {a,_}, {_,_}]), do: :four_of_a_kind 
+
+#  full_house
+#  flush
+#  straight
+#  three_of_a_kind
+#  two_pair
+#  one_pair
+#  high_card
+
+  def name_hand(_), do: :high_card
+
+  # if hand1_rank > hand2_rank
+  #   return hand1 wins
+  # else if hand2_rank > hand1_rank
+  #   return hand2 wins
+  # else
+  #   compare kickers (compare all five starting from left?)
+  def write_which_kicker_wins do
+     IO.puts "Nobody knows"
   end
 end
--- test/dealer_test.exs
+++ test/dealer_test.exs
@@ -9,4 +9,58 @@ defmodule DealerTest do
     unique_cards = Enum.uniq(Dealer.deck)
     assert Enum.count(unique_cards) == Enum.count(Dealer.deck)
   end
+
+  test "name of straight flush matches royal flush" do
+    royal_flush = [{14,:hearts},{13,:hearts},{12,:hearts},{11,:hearts},{10,:hearts}]
+    assert Dealer.name_hand(royal_flush) == :straight_flush
+  end
+
+  test "name of normal straight is not straight flush" do
+    straight = [{14,:hearts},{13,:clubs},{12,:hearts},{11,:hearts},{10,:hearts}]
+    refute Dealer.name_hand(straight) == :straight_flush
+  end
+
+  test "normal flush is not straight flush" do
+    straight = [{2,:hearts},{13,:hearts},{12,:hearts},{11,:hearts},{10,:hearts}]
+    refute Dealer.name_hand(straight) == :straight_flush
+  end
+
+  test "names low straight flush correctly" do 
+    low_straight_flush = [{14,:hearts},{5,:hearts},{4,:hearts},{3,:hearts},{2,:hearts}]
+    assert Dealer.name_hand(low_straight_flush) == :straight_flush
+  end
+
+  test "names four of a kind" do
+    four_of_a_kind = [{14,:hearts},{14,:clubs},{14,:spades},{14,:diamonds},{2,:hearts}]
+    assert Dealer.name_hand(four_of_a_kind) == :four_of_a_kind
+  end
+
+  # Test dealing
+  test "deal six players returns six hands of five cards" do
+    six_players = 6
+    no_cards_required = six_players * 5
+    #deck = fake_deck
+    deck = Dealer.deck |> Enum.shuffle
+
+    dealt_hands = Dealer.deal(six_players, deck)
+
+    for hand <- dealt_hands do
+      assert Enum.count(hand) == 5
+    end
+
+    #five_per_hand? = fn(hand) ->
+      #number_of_cards = hand |> Enum.count 
+      #assert number_of_cards == 5
+    #end
+    #dealt_hands |> Enum.all? five_per_hand?/1
+
+    number_of_hands = dealt_hands |> Enum.count
+    assert number_of_hands == six_players
+
+    #for hand <- dealt_hands, do
+      #IO.puts "Got #{hand}"
+    #end
+    #for hand <- dealt_hands, do:
+      #assert(Enum.count <- hand == 5)
+  end
 end
