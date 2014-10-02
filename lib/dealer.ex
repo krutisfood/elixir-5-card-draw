b5c50eefca5ccd51ef0d1df4bd300ddc19597692
Kurt Gardiner <k.a.gardiner@gmail.com>
2014-10-03 09:29:13 +1000

    Almost comparing, needs to accurately compare five high/ace low flush

--- lib/dealer.ex
+++ lib/dealer.ex
@@ -15,60 +15,58 @@ defmodule Dealer do
   end
 
   # straight flush
-  def name_hand([{a,s}, {b,s}, {c,s}, {d,s}, {e,s}]) when a == (b+1) and b == (c+1) and c == (d+1) and d == (e+1), do: :straight_flush
-  def name_hand([{a,s}, {b,s}, {c,s}, {d,s}, {e,s}]) when a == 14 and b == 5 and b == (c+1) and c == (d+1) and d == (e+1), do: :straight_flush
+  def hand_rank([{a,s}, {b,s}, {c,s}, {d,s}, {e,s}]) when a == (b+1) and b == (c+1) and c == (d+1) and d == (e+1), do: 9
+  def hand_rank([{a,s}, {b,s}, {c,s}, {d,s}, {e,s}]) when a == 14 and b == 5 and b == (c+1) and c == (d+1) and d == (e+1), do: 9
 
   # four of a kind
-  def name_hand([{a,_}, {a,_}, {a,_}, {a,_}, {_,_}]), do: :four_of_a_kind 
+  def hand_rank([{a,_}, {a,_}, {a,_}, {a,_}, {_,_}]), do: 8
 
   #  full house
-  def name_hand([{a,_}, {a,_}, {a,_}, {b,_}, {b,_}]), do: :full_house
-  def name_hand([{a,_}, {a,_}, {b,_}, {b,_}, {b,_}]), do: :full_house
+  def hand_rank([{a,_}, {a,_}, {a,_}, {b,_}, {b,_}]), do: 7
+  def hand_rank([{a,_}, {a,_}, {b,_}, {b,_}, {b,_}]), do: 7
 
   #  flush
-  def name_hand([{_,a}, {_,a}, {_,a}, {_,a}, {_,a}]), do: :flush
+  def hand_rank([{_,a}, {_,a}, {_,a}, {_,a}, {_,a}]), do: 6
 
-#  straight
-#  three_of_a_kind
-#  two_pair
-#  one_pair
+  # straight
+  def hand_rank([{a,_}, {b,_}, {c,_}, {d,_}, {e,_}]) when a == (b+1) and b == (c+1) and c == (d+1) and d == (e+1), do: 5
+  def hand_rank([{a,_}, {b,_}, {c,_}, {d,_}, {e,_}]) when a == 14 and b == 5 and b == (c+1) and c == (d+1) and d == (e+1), do: 5
+
+  # three_of_a_kind
+  def hand_rank([{a,_}, {a,_}, {a,_}, {_,_}, {_,_}]), do: 4 
+  def hand_rank([{_,_}, {a,_}, {a,_}, {a,_}, {_,_}]), do: 4 
+  def hand_rank([{_,_}, {_,_}, {a,_}, {a,_}, {a,_}]), do: 4 
+
+  # two_pair
+  def hand_rank([{a,_}, {a,_}, {b,_}, {b,_}, {_,_}]), do: 3 
+  def hand_rank([{a,_}, {a,_}, {_,_}, {b,_}, {b,_}]), do: 3 
+
+  # one_pair
+  def hand_rank([{a,_}, {a,_}, {_,_}, {_,_}, {_,_}]), do: 2
+  def hand_rank([{_,_}, {a,_}, {a,_}, {_,_}, {_,_}]), do: 2
+  def hand_rank([{_,_}, {_,_}, {a,_}, {a,_}, {_,_}]), do: 2
+  def hand_rank([{_,_}, {_,_}, {_,_}, {a,_}, {a,_}]), do: 2
 
   # high card
-  def name_hand(_), do: :high_card
+  def hand_rank(_), do: 1
 
-  def declares_defeats(winner, hand2) do
-    winner |> name_hand |> hand_rank
-    hand2 |> name_hand |> hand_rank
+  def name_hand(hand), do: name_hand_rank(hand_rank(hand))
+  def name_hand_rank(rank) when rank == 9, do: :straight_flush
+  def name_hand_rank(rank) when rank == 8, do: :four_of_a_kind
+  def name_hand_rank(rank) when rank == 7, do: :full_house
+  def name_hand_rank(rank) when rank == 6, do: :flush
+  def name_hand_rank(rank) when rank == 5, do: :straight
+  def name_hand_rank(rank) when rank == 4, do: :three_of_a_kind
+  def name_hand_rank(rank) when rank == 3, do: :two_pair
+  def name_hand_rank(rank) when rank == 2, do: :pair
+  def name_hand_rank(_), do: :high_card
 
-    compare_types
-    compare_kickers(winner, hand2)
-  end
 
-  def compare_kickers([ hand1_card | rest_of_hand1 ], [ hand2_card | rest_of_hand2 ]) do
-    cond do
-      hand1_card |> is_higher_than(hand2_card) ->
-        true
-      true ->
-        compare_kickers(rest_of_hand1, rest_of_hand2)
-    end
-  end
+  def declares_defeats(winner, hand2), do: _declares_defeats({winner, hand_rank(winner)}, {hand2,hand_rank(hand2)})
+  def _declares_defeats({_, winner_rank}, {_, hand2_rank}) when winner_rank > hand2_rank, do: true
+  def _declares_defeats({winner, _}, {hand2, _}), do: compare_kickers(winner, hand2)
 
-  def compare_kickers([], []) do
-    false
-  end
-
-  def is_higher_than({value_card1, suit1}, {value_card2, suit2}) do
-    IO.puts "Checking if #{value_card1} > #{value_card2}"
-    value_card1 > value_card2
-  end
-
-  # if hand1_rank > hand2_rank
-  #   return hand1 wins
-  # else if hand2_rank > hand1_rank
-  #   return hand2 wins
-  # else
-  #   compare kickers (compare all five starting from left?)
-  def write_which_kicker_wins do
-     IO.puts "Nobody knows"
-  end
+  def compare_kickers([ {card1_value,_} | _ ], [ {card2_value,_} | _ ]) when card1_value > card2_value , do: true
+  def compare_kickers([ _ | rest_of_hand1 ], [ _ | rest_of_hand2 ]), do: compare_kickers(rest_of_hand1, rest_of_hand2)
+  def compare_kickers([], []), do: false
 end
--- test/dealer_test.exs
+++ test/dealer_test.exs
@@ -1,6 +1,7 @@
 defmodule DealerTest do
   use ExUnit.Case
 
+  # Test Deck
   test "the deck has 52 cards in total" do
     assert Enum.count(Dealer.deck) == 52
   end
@@ -10,6 +11,7 @@ defmodule DealerTest do
     assert Enum.count(unique_cards) == Enum.count(Dealer.deck)
   end
 
+  # Test hand names
   test "name of straight flush matches royal flush" do
     royal_flush = [{14,:hearts},{13,:hearts},{12,:hearts},{11,:hearts},{10,:hearts}]
     assert Dealer.name_hand(royal_flush) == :straight_flush
@@ -50,25 +52,133 @@ defmodule DealerTest do
     assert Dealer.name_hand(heart_flush) == :flush
   end
 
-  # Test some draws
+  test "names a series of consecutive numbers with different suits a straight" do
+    straight = [{14,:hearts},{13,:clubs},{12,:hearts},{11,:hearts},{10,:hearts}]
+    assert Dealer.name_hand(straight) == :straight
+  end
+
+  test "names an ace low series of consecutive numbers with different suits a straight" do
+    ace_low_straight = [{14,:hearts},{5,:spades},{4,:hearts},{3,:hearts},{2,:hearts}]
+    assert Dealer.name_hand(ace_low_straight) == :straight
+  end
+
+  test "names a hand with first three cards the same value three of a kind" do
+    three_of_a_kind = [{14,:hearts},{14,:spades},{14,:clubs},{3,:hearts},{2,:hearts}]
+    assert Dealer.name_hand(three_of_a_kind) == :three_of_a_kind
+  end
+
+  test "names a hand with second third and fourth cards the same value three of a kind" do
+    three_of_a_kind = [{14,:hearts},{11,:spades},{11,:clubs},{11,:hearts},{2,:hearts}]
+    assert Dealer.name_hand(three_of_a_kind) == :three_of_a_kind
+  end
+
+  test "names a hand with third, fourth and fifth cards the same value three of a kind" do
+    three_of_a_kind = [{14,:hearts},{12,:spades},{11,:clubs},{11,:hearts},{11,:spades}]
+    assert Dealer.name_hand(three_of_a_kind) == :three_of_a_kind
+  end
+
+  test "names a hand with first two cards one value and second two cards another two pair" do
+    two_pair = [{14,:hearts},{14,:spades},{12,:clubs},{12,:hearts},{2,:hearts}]
+    assert Dealer.name_hand(two_pair) == :two_pair
+  end
+
+  test "names a hand with first two cards one value and last two cards another two pair" do
+    two_pair = [{14,:hearts},{14,:spades},{13,:clubs},{12,:hearts},{12,:spades}]
+    assert Dealer.name_hand(two_pair) == :two_pair
+  end
+
+  test "names a hand with first two cards the same a pair" do
+    pair = [{14,:hearts},{14,:spades},{13,:clubs},{12,:hearts},{1,:spades}]
+    assert Dealer.name_hand(pair) == :pair
+  end
+
+  test "names a hand with second and third cards the same value as a pair" do
+    pair = [{14,:hearts},{13,:spades},{13,:clubs},{12,:hearts},{1,:spades}]
+    assert Dealer.name_hand(pair) == :pair
+  end
+
+  test "names a hand with third and fourth cards the same value as a pair" do
+    pair = [{14,:hearts},{13,:spades},{12,:clubs},{12,:hearts},{1,:spades}]
+    assert Dealer.name_hand(pair) == :pair
+  end
+
+  test "names a hand with fourth and fifth cards the same value as a pair" do
+    pair = [{14,:hearts},{13,:spades},{12,:clubs},{1,:hearts},{1,:spades}]
+    assert Dealer.name_hand(pair) == :pair
+  end
+
+
+  # Test hand rank comparison
+  test "jack high straight flush beats ace high flush" do
+    jack_high_straight_flush = [{11,:clubs},{10,:clubs},{9,:clubs},{8,:clubs},{7,:clubs}]
+    ace_high_flush = [{14,:hearts},{13,:hearts},{12,:hearts},{11,:hearts},{6,:hearts}]
+    assert jack_high_straight_flush |> Dealer.declares_defeats ace_high_flush
+  end
+
+  test "jack high straight flush beats aces full of kings " do
+    jack_high_straight_flush = [{11,:clubs},{10,:clubs},{9,:clubs},{8,:clubs},{7,:clubs}]
+    aces_full_of_kings = [{14,:hearts},{14,:clubs},{14,:spades},{13,:diamonds},{13,:hearts}]
+    assert jack_high_straight_flush |> Dealer.declares_defeats aces_full_of_kings
+  end
+
+  test "jack high flush beats ace high straight" do
+    ace_high_straight = [{14,:hearts},{13,:clubs},{12,:hearts},{11,:hearts},{10,:hearts}]
+    jack_high_flush = [{11,:clubs},{8,:clubs},{5,:clubs},{3,:clubs},{1,:clubs}]
+    assert jack_high_flush |> Dealer.declares_defeats ace_high_straight
+  end
+
+  test "ace high straight beats three of a kind" do
+    ace_high_straight = [{14,:hearts},{13,:clubs},{12,:hearts},{11,:hearts},{10,:hearts}]
+    three_of_a_kind = [{11,:clubs},{11,:spades},{11,:hearts},{3,:clubs},{1,:clubs}]
+    assert ace_high_straight |> Dealer.declares_defeats three_of_a_kind
+  end
+
+
+  # Test some comparisons where hands have same rank, e.g. flush vs flush
   test "ace high flush beats jack high flush" do
     ace_high_flush = [{14,:hearts},{8,:hearts},{5,:hearts},{3,:hearts},{1,:hearts}]
     jack_high_flush = [{11,:clubs},{8,:clubs},{5,:clubs},{3,:clubs},{1,:clubs}]
     assert ace_high_flush |> Dealer.declares_defeats jack_high_flush
   end
 
-  # Test some draws
   test "eight high flush does not beat jack high flush" do
     eight_high_flush = [{8,:hearts},{7,:hearts},{5,:hearts},{3,:hearts},{1,:hearts}]
     jack_high_flush = [{11,:clubs},{8,:clubs},{5,:clubs},{3,:clubs},{1,:clubs}]
     refute eight_high_flush |> Dealer.declares_defeats jack_high_flush
   end
 
+  test "aces and twos beats jacks and tens" do
+    aces_and_twos = [{14,:hearts},{14,:spades},{5,:hearts},{2,:hearts},{2,:clubs}]
+    jacks_and_tens = [{11,:hearts},{11,:hearts},{10,:hearts},{10,:clubs},{1,:hearts}]
+    assert aces_and_twos |> Dealer.declares_defeats jacks_and_tens
+  end
+   
+  test "aces low straight is defeated by seven high straight" do
+    ace_low_straight = [{14,:hearts},{5,:spades},{4,:hearts},{3,:hearts},{2,:clubs}]
+    seven_high_straight = [{7,:hearts},{6,:hearts},{5,:hearts},{4,:clubs},{3,:spades}]
+    refute ace_low_straight  |> Dealer.declares_defeats seven_high_straight
+  end
+
+  test "aces low straight flush is defeated by seven high straight flush" do
+    ace_low_straight_flush = [{14,:hearts},{5,:spades},{4,:hearts},{3,:hearts},{2,:clubs}]
+    seven_high_straight_flush = [{7,:hearts},{6,:hearts},{5,:hearts},{4,:clubs},{3,:spades}]
+    refute ace_low_straight_flush |> Dealer.declares_defeats seven_high_straight_flush
+  end
+
+
+
+  # Test some draws
+  test "equivalent two pair hands equal" do
+    hand1 = [{14,:hearts},{14,:spades},{5,:hearts},{2,:hearts},{2,:clubs}]
+    hand2 = [{14,:clubs},{14,:diamonds},{5,:clubs},{2,:diamonds},{2,:spades}]
+    refute hand1 |> Dealer.declares_defeats hand2 
+    refute hand2 |> Dealer.declares_defeats hand1
+  end
+
 
   # Test dealing
   test "deal six players returns six hands of five cards" do
     six_players = 6
-    no_cards_required = six_players * 5
     #deck = fake_deck
     deck = Dealer.deck |> Enum.shuffle
 
@@ -78,13 +188,23 @@ defmodule DealerTest do
       assert Enum.count(hand) == 5
     end
 
-    # Couldn't get this working
-    #has_five_cards? = fn(hand) ->
-      #assert Enum.count(hand) == 5
-    #end
-    #dealt_hands |> Enum.all?(has_five_cards?/2)
-
     number_of_hands = dealt_hands |> Enum.count
     assert number_of_hands == six_players
+  end
+
+  # Test dealing
+  test "deal four players returns four hands of five cards" do
+    four_players = 4
+    #deck = fake_deck
+    deck = Dealer.deck |> Enum.shuffle
+
+    dealt_hands = Dealer.deal(four_players, deck)
+
+    for hand <- dealt_hands do
+      assert Enum.count(hand) == 5
+    end
+
+    number_of_hands = dealt_hands |> Enum.count
+    assert number_of_hands == four_players
   end
 end
